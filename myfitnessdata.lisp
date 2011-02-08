8b704d1e85a9b79a439b41acfad06ce99909612b
Duncan Bayne <dhgbayne@gmail.com>
2011-02-09 08:47:10 +1100

    Broken out login logic so it happens once per run.

--- myfitnessdata.lisp
+++ myfitnessdata.lisp
@@ -3,23 +3,32 @@
 (ql:quickload '("drakma" "closure-html" "cxml-stp"))
 
 (defun show-usage () 
-  (format t "Usage: myfitnessdata USERNAME PASSWORD~%~%")
+  (format t "Usage: myfitnessdata USERNAME PASSWORD PATH~%~%")
   (format t "  USERNAME  Your MyFitnessPal username~%")
-  (format t "  PASSWORD  Your MyFitnessPal password~%~%")
+  (format t "  PASSWORD  Your MyFitnessPal password~%")
+  (format t "  PATH      The path into which to place the CSV files~%~%")
   (format t "Example:~%~%")
-  (format t "  ./myfitnessdata bob b0bsp4ss! weights.csv~%~%"))
+  (format t "  myfitnessdata bob b0bsp4ss! c:\\Users\\bob\\fitness~%~%")
+  (format t "This will log into the MyFitnessPal site with the username 'bob' and the~%")
+  (format t "password 'b0bsp4ss!'.  It will then scrape data into CSV files, and put~%")
+  (format t "those files into the directory '/home/bob/fitness', overwriting existing~%")
+  (format t "files.~%~%"))
 
-(defun get-page (page-num username password)
-  (let ((cookie-jar (make-instance 'drakma:cookie-jar)))
-    (drakma:http-request "http://www.myfitnesspal.com/account/login"
-  			 :method :post
-  			 :parameters `(("username" . ,username) ("password" . ,password))
-  			 :cookie-jar cookie-jar)
-    (let ((url (concatenate 'string "http://www.myfitnesspal.com/measurements/edit?type=1&page=" (write-to-string page-num))))
-      (let ((body (drakma:http-request url :cookie-jar cookie-jar)))
-	(if (search "No measurements found." body)
-	    nil
-	  body)))))
+(defun login (username password)
+  (setq cookie-jar (make-instance 'drakma:cookie-jar))
+  (drakma:http-request "http://www.myfitnesspal.com/account/login"
+		       :method :post
+		       :parameters `(("username" . ,username) ("password" . ,password))
+		       :cookie-jar cookie-jar)
+  cookie-jar)
+
+(defun get-page (page-num cookie-jar)
+  (let ((url (concatenate 'string "http://www.myfitnesspal.com/measurements/edit?type=1&page=" (write-to-string page-num))))
+    (format t "Fetching ~A~%" url)
+    (let ((body (drakma:http-request url :cookie-jar cookie-jar)))
+      (if (search "No measurements found." body)
+	  nil
+	body))))
 
 (defun scrape-body (body)
   (let ((valid-xhtml (chtml:parse body (cxml:make-string-sink))))
@@ -46,15 +55,19 @@
 (defun nth-child-data (number row)
   (stp:data (stp:nth-child 0 (stp:nth-child number row))))
 
-(defun scrape-page (page-num username password)
-  (let ((body (get-page page-num username password)))
-    (if (not (string= nil body))
-	(progn
-	  (append (scrape-body body)
-		  (scrape-page (+ 1 page-num) username password))))))
+(defun scrape-page (page-num cookie-jar)
+  (let ((body (get-page page-num cookie-jar)))
+    (if body
+	(append (scrape-body body)
+		(scrape-page (+ 1 page-num) cookie-jar)))))
+
+(defun write-csv (data path)
+  (format t "Would write CSV to ~A: ~A" path data)) 
 
 (if (= (length sb-ext:*posix-argv*) 3)
-    (let ((username (nth 0 sb-ext:*posix-argv*))
-	  (password (nth 1 sb-ext:*posix-argv*)))
-      (scrape-page 1 username password))
+    (let* ((username (nth 0 sb-ext:*posix-argv*))
+	   (password (nth 1 sb-ext:*posix-argv*))
+	   (path (nth 2 sb-ext:*posix-argv*))
+	   (cookie-jar (login username password)))
+      (write-csv (scrape-page 1 cookie-jar) path))
   (show-usage))
