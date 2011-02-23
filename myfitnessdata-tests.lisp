3f39344efa8b1d90341fa8ab090124690a2eda11
Duncan Bayne <dhgbayne@gmail.com>
2011-02-23 12:16:39 +1100

    Improving tests with macro

--- myfitnessdata-tests.lisp
+++ myfitnessdata-tests.lisp
@@ -4,28 +4,35 @@
 (in-package :myfitnessdata-tests)
 
 (load "myfitnessdata.lisp")
+
 (ql:quickload '("lisp-unit"))
+
+(defmacro define-test-with-cookie-jar (test-name cookie-domain cookie-name test)
+  `(lisp-unit:define-test
+    ,test-name
+    (let* ((cookie-jar (make-instance 'drakma:cookie-jar))
+	   (cookie (make-instance 'drakma:cookie :name ,cookie-name :domain ,cookie-domain)))
+      (setf (drakma:cookie-jar-cookies cookie-jar) (list cookie))
+      ,test)))
 
 (lisp-unit:define-test 
  logged-in-nil-when-no-cookies
  (let ((cookie-jar (make-instance 'drakma:cookie-jar)))
    (lisp-unit:assert-false (myfitnessdata:logged-in? cookie-jar))))
 
-(lisp-unit:define-test
- logged-in-nil-when-cookies-from-wrong-domain
- (let ((cookie-jar (make-instance 'drakma:cookie-jar))
-       (cookie (make-instance 'drakma:cookie :name "known_user" :domain "example.com")))
-   (setf (drakma:cookie-jar-cookies cookie-jar) (list cookie))
-   (lisp-unit:assert-false (myfitnessdata:logged-in? cookie-jar))))
+(define-test-with-cookie-jar
+  logged-in-nil-when-cookies-from-wrong-domain
+  "www.example.com" 
+  "known_user" 
+  (lisp-unit:assert-false (myfitnessdata:logged-in? cookie-jar)))
 
-(lisp-unit:define-test
- logged-in-t-when-proper-cookies
- (let ((cookie-jar (make-instance 'drakma:cookie-jar))
-       (cookie (make-instance 'drakma:cookie :name "known_user" :domain "www.myfitnesspal.com")))
-   (setf (drakma:cookie-jar-cookies cookie-jar) (list cookie))
-   (lisp-unit:assert-true (myfitnessdata:logged-in? cookie-jar))))
+(define-test-with-cookie-jar 
+  logged-in-t-when-cookies-from-right-domain
+  "www.myfitnesspal.com" 
+  "known_user"
+  (lisp-unit:assert-true (myfitnessdata:logged-in? cookie-jar)))
 
 (lisp-unit:run-tests 
  logged-in-nil-when-no-cookies
  logged-in-nil-when-cookies-from-wrong-domain
- logged-in-t-when-proper-cookies)
+ logged-in-t-when-cookies-from-right-domain)
